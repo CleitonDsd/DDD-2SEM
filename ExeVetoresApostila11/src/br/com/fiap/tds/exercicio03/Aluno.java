2f83435cb735fbc43264183995a614a539c60fef
Cleiton Dias <s.diascleiton@gmail.com>
2020-08-08 11:53:08 -0300

    Exe 03 - Apostila 11 | Vetores

---
+++ ExeVetoresApostila11/src/br/com/fiap/tds/exercicio03/Aluno.java
@@ -0,0 +1,45 @@
+package br.com.fiap.tds.exercicio03;
+
+public class Aluno {
+
+	private String nome;
+
+	private int[] notas = new int[3];
+
+	public Aluno() {
+
+	}
+
+	public Aluno(String nome, int[] notas) {
+		super();
+		this.nome = nome;
+		this.notas = notas;
+	}
+
+	public int recuperarMaiorNota() {
+		int maiorNota = notas[0];
+		for (int i = 0; i < notas.length; i++) {
+			if (maiorNota < notas[i]) {
+				maiorNota = notas[i];
+			}
+		}
+		return maiorNota;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int[] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(int[] notas) {
+		this.notas = notas;
+	}
+
+}
