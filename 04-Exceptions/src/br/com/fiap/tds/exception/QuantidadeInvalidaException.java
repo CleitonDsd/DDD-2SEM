53511668633dd09100d9fc2fe22d58abd660842b
Cleiton Dias <s.diascleiton@gmail.com>
2020-09-28 22:46:40 -0300

    04 | Exceptions

---
+++ 04-Exceptions/src/br/com/fiap/tds/exception/QuantidadeInvalidaException.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tds.exception;
+
+public class QuantidadeInvalidaException extends Exception {
+
+	public QuantidadeInvalidaException() {
+		super("Quantidade inválida");
+	}
+	
+	public QuantidadeInvalidaException(String mensagem) {
+		super(mensagem);
+	}
+	
+}
